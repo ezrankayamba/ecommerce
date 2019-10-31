5f63c57a2b8b4addf3339478174e61946cf23c0a
ezrankayamba <ezrankayamba@gmail.com>
2019-10-31 21:19:28 +0300

    Added reactjs

---
+++ products/static/products/js/cart.js
@@ -0,0 +1,66 @@
+'use stri ct';
+
+const e = React.createElement;
+class MainCart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { products: [] };
+    }
+
+    addToCart(prod) {
+        console.log(prod)
+        let found = false
+        let tmpProds = this.state.products
+        for (let i = 0; i < tmpProds.length; i++) {
+            let p = tmpProds[i]
+            if (p.id === prod.id) {
+                tmpProds[i] = { id: prod.id, count: prod.count }
+                found = true
+                break;
+            }
+        }
+        if (!found) {
+            tmpProds.push({ id: prod.id, count: prod.count })
+        }
+        this.setState({ products: tmpProds })
+    }
+
+    render() {
+        let map = this.state.products.map(p => p.count)
+        console.log(map)
+        let t = map.reduce((a, b) => a + b, 0)
+        console.log(t)
+        return e('span', {}, t)
+    }
+}
+let mainCartDiv = document.querySelector('.cart-counter')
+const mainCart = ReactDOM.render(e(MainCart, {}), mainCartDiv)
+class ProductCart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { id: props.id, cart: 0 };
+    }
+
+    render() {
+        return e(
+            'a', {
+                onClick: () => {
+                    let count = this.state.cart + 1
+                    console.log('Cart: ', this.state.id, ' - ', count)
+                    this.setState({ cart: count })
+                    mainCart.addToCart({ id: this.state.id, count: count })
+                },
+                className: 'btn',
+                role: 'button'
+            },
+            e('i', {
+                className: 'fas fa-cart-plus'
+            })
+        );
+    }
+}
+
+const list = document.querySelectorAll('.cart-btn-wrap')
+list.forEach((el) => {
+    ReactDOM.render(e(ProductCart, { id: el.dataset.id }), el);
+})
