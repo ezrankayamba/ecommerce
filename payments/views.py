cd929e589c7864d7503bb240060c159ed7a10dca
ezrankayamba <ezrankayamba@gmail.com>
2019-11-12 16:55:30 +0300

    UIX fixes

--- payments/views.py
+++ payments/views.py
@@ -9,14 +9,19 @@ from django.views.generic.edit import UpdateView
 from . import forms
 
 
-class ProductsHomeView(ListView):
-    model = models.Product
-    context_object_name = 'products'
-    template_name = 'products/home.html'
+class PaymnetsHomeView(LoginRequiredMixin, ListView):
+    model = models.Payment
+    context_object_name = 'payments'
+    template_name = 'payments/home.html'
     paginate_by = 6
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return models.Payment.objects.filter()
+        else:
+            return models.Payment.objects.filter(order__user=user)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['product_images'] = models.ProductImage.objects.all()
-        context['top_products'] = models.Product.objects.all()[:2]
         return context
